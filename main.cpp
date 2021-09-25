70eb882cd003d6e1f24337c7752e221ba0bb1343
LeftTry <lerest.go@gmail.com>
2021-09-25 10:22:10 +0300

    Added len

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -21,41 +22,47 @@ public:
     void set_x(double _x) { x = _x; }
     void set_y(double _y) { y = _y; }
 
-    vector operator+(vector);
+    vector operator+(vector) const;
 
-    vector operator-(vector);
+    vector operator-(vector) const;
 
-    double operator*(vector);
+    double operator*(vector) const;
 
-    vector operator/(double);
+    vector operator/(double) const;
 
-    vector operator*(double);
+    vector operator*(double) const;
 
-    double operator%(vector);
+    double operator%(vector) const;
+
+    double len() const;
 };
 
-vector vector::operator+(vector a) {
-    return vector(x + a.x, y + a.y);
+vector vector::operator+(vector a) const {
+    return {x + a.x, y + a.y};
 }
 
-vector vector::operator-(vector a) {
-    return vector(x - a.x, y - a.y);
+vector vector::operator-(vector a) const {
+    return {x - a.x, y - a.y};
 }
 
-double vector::operator*(vector a) {
+double vector::operator*(vector a) const {
     return a.x * x + a.y * y;
 }
 
-vector vector::operator/(double k) {
-    return vector(x / k, y / k);
+vector vector::operator/(double k) const {
+    return {x / k, y / k};
 }
 
-vector vector::operator*(double k) {
-    return vector(k * x, k * y);
+vector vector::operator*(double k) const {
+    return {k * x, k * y};
 }
 
-double vector::operator%(vector a) {
+double vector::operator%(vector a) const {
     return x * a.y - y * a.x;
+}
+
+double vector::len() const {
+    return sqrt(x * x + y * y);
 }
 
 int main() {
